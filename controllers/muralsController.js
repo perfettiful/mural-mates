11b33e6b292af571efddbfe2bf9876cb15ce6a54
unknown <benjamin.rademacher@gmail.com>
2018-08-01 21:59:08 -0500

    fixed image post form submission

--- controllers/muralsController.js
+++ controllers/muralsController.js
@@ -21,6 +21,7 @@ module.exports = {
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
+    console.log(req.body);
     db.Mural
       .create(req.body)
       .then(dbModel => res.json(dbModel))
