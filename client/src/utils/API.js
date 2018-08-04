c717d5f96572e6b737c4b53386779e8f9b0e906c
unknown <benjamin.rademacher@gmail.com>
2018-08-04 11:35:26 -0500

    changed App.js inside src folder to have routes with '/api/murals/'

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,19 +3,19 @@ import axios from "axios";
 export default {
   // Gets all murals
   getMurals: function() {
-    return axios.get("/api/");
+    return axios.get("/api/murals/");
   },
   // Gets the mural with the given id
   getMural: function(id) {
-    return axios.get("/api/" + id);
+    return axios.get("/api/murals/" + id);
   },
   // Deletes the mural with the given id
   deleteMural: function(id) {
-    return axios.delete("/api/" + id);
+    return axios.delete("/api/murals/" + id);
   },
   // Saves a mural to the database
   saveMural: function(muralData) {
-    return axios.post("/api/", muralData);
+    return axios.post("/api/murals/", muralData);
   },
   logMural:function(muralData){
     console.log(muralData);
