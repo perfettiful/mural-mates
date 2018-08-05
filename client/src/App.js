ba072e8dbfdb8307556a2053ce1ab3006e59efa3
Nate <nathanperfetti@gmail.com>
2018-08-05 18:24:43 -0500

    A seeminly innert Auth0 authentication using Google that appears to not bar app usage or interfere with other compnoents currently

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {Component} from "react";
+import { Navbar, Button } from 'react-bootstrap';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Books from "./pages/Books";
 import Home from "./pages/Home";
@@ -6,19 +7,84 @@ import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 
-const App = () => (
-  <Router>
-    <div>
-      <Nav />
-      <Switch>
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/books" component={Books} />
-        <Route exact path="/books/:id" component={Detail} />
-        <Route component={NoMatch} />
-      </Switch>
-    </div>
-  </Router>
-);
+// App.js
+import Auth from './Auth/Auth.js';
+
+const auth = new Auth();
+
+class App extends Component {
+  componentDidMount(){
+    
+    // if (!auth.isAuthenticated()){
+      
+      //   auth.login();
+      // } 
+    }
+    
+    goTo(route) {
+      this.props.history.replace(`/${route}`)
+  }
+  
+  login() {
+    this.props.auth.login();
+  }
+  
+  logout() {
+    this.props.auth.logout();
+  }
+  
+  render() {
+    const isAuthenticated = auth.isAuthenticated;
+    
+    return (
+      <div>
+      <Navbar.Header>
+        <Navbar fluid>
+            <Navbar.Brand>
+              <a href="#">Auth0 - React</a>
+            </Navbar.Brand>
+            <Button
+              bsStyle="primary"
+              className="btn-margin"
+              onClick={this.goTo.bind(this, 'home')}
+              >
+              Home
+            </Button>
+            {
+              !isAuthenticated() && (
+                <Button
+                bsStyle="primary"
+                className="btn-margin"
+                onClick={this.login.bind(this)}
+                >
+                    Log In
+                  </Button>
+                )
+              }
+            {
+              isAuthenticated() && (
+                <Button
+                bsStyle="primary"
+                className="btn-margin"
+                onClick={this.logout.bind(this)}
+                >
+                    Log Out
+                  </Button>
+                )
+              }
+          </Navbar>
+        </Navbar.Header>
+            <Switch>
+                 <Route exact path="/home" component={Home} />
+                 <Route exact path="/" component={Home} />
+                 <Route exact path="/books" component={Books} />
+                 <Route exact path="/books/:id" component={Detail} />
+                 <Route component={NoMatch} />
+              </Switch> 
+      </div>
+    );
+  }
+}
+
 
 export default App;
