5707cce4689ac2a07ea4fbc97518235a066f6a31
zac keilholz <keilh001@umn.edu>
2018-08-05 21:32:57 -0500

    Cleaned up function in Home page and added link to Start a New Game

--- client/src/pages/Game/Game.js
+++ client/src/pages/Game/Game.js
@@ -16,18 +16,21 @@ class Game extends Component {
 
     pImg2: "",
     gameId: "",
-    private:false
+    private: false
   };
 
   // When this component mounts, grab the book with the _id of this.props.match.params.id
   // e.g. localhost:3000/books/599dcb67f0f16317844583fc
   componentDidMount() {
     API.getMural(this.props.match.params.id)
-      .then(res =>{ this.setState({ 
-        pImg1: res.data.pImg1, title: res.data.title, gameId: res.data.uniqueid }) 
-        console.log(this.state)})
+      .then(res => {
+        this.setState({
+          pImg1: res.data.pImg1, title: res.data.title, gameId: res.data.uniqueid
+        })
+        console.log(this.state)
+      })
       .catch(err => console.log(err));
-     
+
   }
 
 
@@ -48,13 +51,13 @@ class Game extends Component {
 
       //Take the returned data and as a demonstration of pulling info from mongo and rendering it, add this res.data stuff to the current state
     }).then(res =>
-      this.setState({ pImg2:res.data.pImg1, title:res.data.title, gameId:res.data._id })
-    
+      this.setState({ pImg2: res.data.pImg1, title: res.data.title, gameId: res.data._id })
+
     )
-    //Mongo Error handling
+      //Mongo Error handling
       .catch(err => console.log((err))
-      )  
-      console.log(this.state.gameId);
+      )
+    console.log(this.state.gameId);
 
   };
 
@@ -87,12 +90,12 @@ class Game extends Component {
         {/* Display returned data from mongo submission- this is what user 1 and 2 will see */}
 
         {/* The URL link we will need to display- obviously this will need to get cleaned up */}
-       <button><a href={'localhost:3000/game/'+this.state.gameId}>Game Url</a></button>
+        <button><a href={'localhost:3000/game/' + this.state.gameId}>Game Url</a></button>
 
         {/* Render the sent image from user A on the page- this is the image pulled from the mongo object */}
         <p>IMG B Image and  Title Loaded: {this.state.title}</p>
         <img src={this.state.pImg2} />
-<p>mongodblink.com/{this.state.gameId}</p>
+        <p>mongodblink.com/{this.state.gameId}</p>
 
 
       </div>
