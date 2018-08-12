43a9fe94c6af0bfa3f988d0cea7ea4c23a4235c8
zac keilholz <keilh001@umn.edu>
2018-08-12 13:18:15 -0500

    Pulling player1 profile info onto continuegame page, and am sending player2 info when submit button is pressed

--- client/src/pages/ContinueGame/ContinueGame.js
+++ client/src/pages/ContinueGame/ContinueGame.js
@@ -13,25 +13,42 @@ import {
 } from "semantic-ui-react";
 import StitchPic from "../../components/StitchPic";
 
-class ContinueGame extends Component {
-  state = {
-    //Player 1 State/Submission Data
-    gameId:"",
-    title: "",
-    pImg1: "",
-    pImg2: ""
-  };
+class ContinueGame extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      profile: {},
+      title: "",
+      pImg1: "",
+      pImg2: "",
+      gameId: "",
+      private: false
+    };
+  }
+
+  componentWillMount() {
+    this.checkAndUpdateState(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.checkAndUpdateState(nextProps);
+  }
+
+  checkAndUpdateState(props) {
+    this.setState({ profile: props.profile, loggedIn: props.loggedIn });
+  }
 
   // When this component mounts, grab the book with the _id of this.props.match.params.id
-  // e.g. localhost:3000/books/599dcb67f0f16317844583fc
   componentDidMount() {
     API.getMural(this.props.match.params.id)
       .then(res => {
         this.setState({
           pImg1: res.data.pImg1,
+          playerName1: res.data.playerName1,
+          playerPhoto1: res.data.playerPhoto1,
           pImg2: res.data.pImg2,
           title: res.data.title,
-          gameId:this.props.match.params.id
+          gameId: this.props.match.params.id
         });
         console.log(this.state);
       })
@@ -54,26 +71,42 @@ class ContinueGame extends Component {
     //Send All user 1 info to mongo
     API.editMural(
       this.state.gameId,
-      {pImg2:canvasDownload}
+      {
+        pImg2: canvasDownload,
+        playerId2: this.props.profile.sub,
+        playerName2: this.props.profile.given_name,
+        playerPhoto2: this.props.profile.picture,
+      }
 
       //Take the returned data and as a demonstration of pulling info from mongo and rendering it, add this res.data stuff to the current state
-    )      .then(res =>
-        this.setState({
-          pImg1: res.data.pImg1,
-          pImg2: res.data.pImg2,
-          title: res.data.title,
-          id: res.data._id,
-          private: res.data.private
-        })
-      )
+    ).then(res =>
+      this.setState({
+        pImg1: res.data.pImg1,
+        pImg2: res.data.pImg2,
+        title: res.data.title,
+        id: res.data._id,
+        private: res.data.private
+      })
+    )
       //Mongo Error handling
       .catch(err => console.log(err));
     console.log(this.state);
   };
 
   render() {
+
+    // User Profile Object
+    const { profile } = this.props;
+
     return (
       <div>
+
+        {/* Pull info about creator of the mural */}
+        <h1><Image src={this.state.playerPhoto1} alt={this.state.playerName1} avatar />{this.state.playerName1} Sent you a Mural!</h1>
+        <br />
+
+        {/* Original Title of Mural */}
+        <h1><i>{this.state.title}</i></h1>
         <Container>
           <Image src={this.state.pImg1} />
           <DrawApp />
@@ -83,11 +116,11 @@ class ContinueGame extends Component {
         </Container>
         {/* Display returned data from mongo submission- this is what user 1 and 2 will see */}
         <Image src={this.state.pImg1} />
-       <Image src={this.state.pImg2} />
+        <Image src={this.state.pImg2} />
 
         {/* The URL link we will need to display- obviously this will need to get cleaned up */}
         <button>
-        <Link to={`/game/mural/${this.state.gameId}`}><span>Final Mural Link</span></Link>
+          <Link to={`/game/mural/${this.state.gameId}`}><span>Final Mural Link</span></Link>
         </button>
 
         {/* Render the sent image from user A on the page- this is the image pulled from the mongo object */}
